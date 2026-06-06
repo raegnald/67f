551eba2042905a67ce2fa5c18f58794cf92301ca
Efrén Javaloyes Sáez <efrenjs82006@gmail.com>
2026-06-06 12:45:37 +0200

    fix: resolve memory management bugs
    Also added the \n character in the 67.67 test file at the end of it to make the output clean.

--- 67f.c
+++ 67f.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #define VEC_LEN 676767
-unsigned char memory[VEC_LEN];
+unsigned char memory[VEC_LEN] = {0};
 int pointer = 0, current = 0;
-char program[676767];
+char program[676767] = {0};
 int errP(){
   printf("Error: pointer is zero\n");
   return 6767;
@@ -12,7 +13,16 @@ int err6(){
   printf("You don't know how to comment? Type /67 ... 67/ and solved.\n");
   return 6767;
 }
+int errArg(){
+  printf("Error. Usage is: ./67f fileName\n");
+  return 6767;
+}
 int main(int argc, char* argv[]) {
+  if(argc != 2){
+    int e = errArg();
+    return e;
+  }
+  bool cont = true; // For the comment section
   char* fileName = argv[1];
   int length = strlen(fileName);
   if(fileName[length-2] != '6' && fileName[length-1] != '7') {
@@ -25,45 +35,49 @@ int main(int argc, char* argv[]) {
   while((c = fgetc(f)) != EOF)
     program[i++] = c;
   program[i] = '\0';
+  fclose(f);
   while (program[current] != '\0') {
     int loop_count;
     char operation = program[current];
-    /* printf("%c", program[current]); */
     
     switch (operation){
-    case '/':
+    case '/': {
       current++;
       int e;
       if(program[current] == '6'){
 	current++;
 	if(program[current] == '7'){
 	  current++;
-	  while(1){
+	  cont = true;
+	  while(program[current] != '\0' && cont){
 	    current++;
-	    if(program[current] == '6'){
-	      if(program[++current] == '7' && program[++current] == '/') break;
+	    if(program[current] == '6' && program[current+1] == '7' && program[current+2] == '/') cont = false;
 	    }
-	  }
+	  if(cont){
+	    e =  err6();
+	    return e;
+	  } else current+=3;
 	} else{
 	  e =  err6();
 	  return e;
 	}
       } else{
-	  e =  err6();
-	  return e;
-	}
-      current++;
+	e =  err6();
+	return e;
+      }
       break;
+    }
     case '6':
       pointer++;
       current++;
       break;
-    case '7':
-      int f = 0;
-      pointer == 0 ? f = errP() : pointer--;
+    case '7': {
+      int f_err = 0;
+      pointer == 0 ? f_err = errP() : pointer--;
       current++;
-      if(f != 0) return f;
+      if(f_err != 0) return f_err;
       break;
+    }
     case '+':
       memory[pointer]++;
       current++;
